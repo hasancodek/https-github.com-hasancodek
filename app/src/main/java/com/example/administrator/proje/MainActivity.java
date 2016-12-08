20e61e217a2aa60329ec074c98b109f4df920437
120201047 <120201047@kocaeli.edu.tr>
2016-12-08 21:26:59 +0200

    :)

--- app/src/main/java/com/example/administrator/proje/MainActivity.java
+++ app/src/main/java/com/example/administrator/proje/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hgfdyfghklhgfdsfghj
+        //deneme ali
+    }
+    public void deneme1(){
+        int x;
     }
 }
